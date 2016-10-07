b07b6aa906cfead67f250df35cd8bf32f4cf862f
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-10-07 13:47:03 +0200

    Made it easier to take ownership over the server entry point
    This by not having to provide createRoutes and createStore by default.

--- extensions/roc-package-web-app-react/app/default/server.js
+++ extensions/roc-package-web-app-react/app/default/server.js
@@ -1,10 +1,3 @@
 import { createServer, useReact } from '../server';
 
-import getRoutesAndStore from './get-routes-and-store';
-
-const { store, routes } = getRoutesAndStore();
-
-useReact(createServer)({
-    createRoutes: routes,
-    createStore: store,
-}).start();
+useReact(createServer)().start();
